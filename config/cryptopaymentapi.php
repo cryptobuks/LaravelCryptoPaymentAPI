b278b46277348a4fc860a4b8b09c8db9bc19bf2d
Omnitasker <admir.alagic@gmail.com>
2018-10-03 14:17:58 +0200

    payment gateway service

--- config/cryptopaymentapi.php
+++ config/cryptopaymentapi.php
@@ -6,4 +6,6 @@ return [
      */
     'api-key' => env('API_KEY_TOKEN', null),
     'api-secret' => env('API_KEY_SECRET', null),
+    'payment-gateway-api-key' => env('PAYMENT_GATEWAY_API_URL', 'orangegateway.demo.ba'),
+    'payment-gateway-api-scheme' => env('PAYMENT_GATEWAY_API_SCHEME', 'http'),
 ];
---
+++ src/PaymentGatewayAPI.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace Omnitask\CryptoPaymentAPI;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\TransferException;
+use Ramsey\Uuid\Uuid;
+use Exception;
+use Log;
+
+class PaymentGatewayAPIResponse {
+    public $success;
+    public $httpStatus;
+    public $responseBody;
+    public $code;
+    public $message;
+
+    public function __construct($success, $httpStatus, $responseBody, $message = "") {
+        $this->success = $success;
+        $this->httpStatus = $httpStatus;
+        $this->responseBody = $responseBody;
+        $this->message = $message;
+    }
+}
+
+class PaymentGatewayAPI
+{
+    protected $apiScheme;
+    protected $apiUrl;
+
+    public function __construct() {
+        $this->apiScheme = config('payment-gateway-api-scheme');
+        $this->apiUrl = config('payment-gateway-api-key');
+    }
+    
+    public function paymentConfirmed($payment) {
+        $url = "/hooks/confirm";
+        Log::info('PaymentGatewayAPI paymentConfirmed ' . $payment->user->email. ' case ID:'.$payment->case_id);
+        $data['case_id'] = $payment->case_id;
+        $data['approved'] = 'yes';
+        $user  = $payment->user;
+        $hash = $payment->amount . $payment->id . $payment->shopPlugin->api_secret;
+        $data['hash'] = $hash;
+         
+        $result = $this->execute($url, [], $data, "get");
+        Log::info('PaymentGatewayAPI paymentConfirmed response: ' . print_r($result, true));
+        return $this->processResult($result, null, "Failed to register user to PaymentGatewayAPI");
+    }
+    
+
+    private function constructFullUrl($url, $params) {
+        $fullUrl =  $this->apiScheme . "://" . $this->apiUrl . '/'  . $url;
+
+        Log::info('PaymentGatewayAPI request url: ' . print_r($fullUrl, true));
+        return $fullUrl;
+    }
+
+    private function execute($url, $headers, $data, $requestType = 'post')
+    {
+        Log::info('PaymentGatewayAPI try: ' . $url);
+        $headers['CONTENT_TYPE'] = 'application/json';
+        $httpClient = new Client(['headers' => $headers, 'verify' => false]);
+      
+        if ($requestType == 'get') {
+            $params = '?';
+            foreach ($data as $key => $obj) {
+                $params .= $key . '=' . $obj;
+                if ($obj !== end($data)) {
+                    $params .= '&';
+                }
+            }
+            $request = $httpClient->get($this->constructFullUrl($url, $params), [
+                'http_errors' => false
+            ]);
+        } else {
+            try {
+                $request = $httpClient->request('POST', $this->constructFullUrl($url, false), ['json' => $data,  'http_errors' => false]);
+ 
+                $result = [
+                    'status' => $request->getStatusCode(),
+                    'body' => json_decode($request->getBody()->getContents(), true)
+                ];
+                
+
+            } catch (\Exception   $e) {
+                Log::info('PaymentGatewayAPI network error', ['result' => $e]);
+                $result = [
+                    'status' => 504,
+                    'body' => 'Error while connecting to PaymentGatewayAPI.'
+                ];
+                return $result;
+            }
+        }
+
+        Log::info('PaymentGatewayAPI: ', ['data' => $data]);
+        Log::info('PaymentGatewayAPI request: ' . print_r($request, true));
+        $result = [
+            'status' => $request->getStatusCode(),
+            'body' => json_decode($request->getBody(), true)
+        ];
+
+        return $result;
+    }
+
+    private function processResult($result, $successMessage = null, $errorMessage = null)
+    {
+        Log::info('resuu', ['result' => $result] );
+        $status = intval($result["status"]);
+        $body = $result['body'];
+        Log::info('processResult', ['body' => $body]);
+        if ($status == 200 || $status == 201) {
+            $response = [
+                'status' => 200,
+                'body' => $body,
+            ];
+
+            if ($successMessage)
+                Log::info($successMessage, (array)$body);
+        } else {
+            Log::error('Result status: '.$status.'. PaymentGatewayAPI Error message: ' . $errorMessage);
+
+            if($status == 400){
+                $message = $result['body']['message'];
+                Log::error('PaymentGatewayAPI Validation Error, message: ' . $message);
+            } else {
+                $status = 500;
+                $message = 'Failed to get response from PaymentGatewayAPI.';
+            }
+            
+            
+            $response = [
+                'status' => $status,
+                'body' => ['error' => $message]
+            ];
+        }
+
+        return $response;
+    }
+}

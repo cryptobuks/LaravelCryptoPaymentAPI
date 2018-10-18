9e61d12ba29331c4174cde65f29f68cd29473032
Omnitasker <admir.alagic@gmail.com>
2018-10-18 13:12:54 +0200

    config

--- config/cryptopaymentapi.php
+++ config/cryptopaymentapi.php
@@ -6,8 +6,8 @@ return [
      */
     'api-key' => env('PAYMENT_GATEWAY_API_KEY', null),
     'api-secret' => env('PAYMENT_GATEWAY_API_SECRET', null),
-    'payment-gateway-api-url' => env('PAYMENT_GATEWAY_API_URL', 'orangegateway.demo.ba'),
-    'payment-gateway-api-scheme' => env('PAYMENT_GATEWAY_API_SCHEME', 'http'),
+    'payment-gateway-api-url' => env('PAYMENT_GATEWAY_API_URL', 'form.migpayments.com'),
+    'payment-gateway-api-scheme' => env('PAYMENT_GATEWAY_API_SCHEME', 'https'),
     'ok-url' => env('PAYMENT_GATEWAY_API_OK_URL', ''),
     'fail-url' => env('PAYMENT_GATEWAY_API_FAIL_URL', ''),
     'confirm-url' => env('PAYMENT_GATEWAY_API_CONFIRM_URL', ''),
